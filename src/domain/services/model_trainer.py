6dfa3ceae46e1ad7a3c674b4b30c09d795dbac90
jethronap <efthioa@gmail.com>
2025-12-23 16:01:26 +0200

    Added return types (#18)

--- src/domain/services/model_trainer.py
+++ src/domain/services/model_trainer.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class ModelTrainer(ABC):
     @abstractmethod
-    def train(self, train_loader, val_loader, device):
+    def train(self, train_loader: Any, val_loader: Any, device: Any) -> None:
         """
         Train the model using the training and validation data loaders on the specified device.
         :param train_loader: Training dataloader
